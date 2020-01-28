1e33b145037111c7788a84306630d1ee6c68790d
Unknown <Ghost27@live.co.uk>
2020-01-28 15:18:07 +0000

    Blank Project Initialisation
    Testing the Blank Project distribution

---
+++ Source/BlankBoats/BlankBoatsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BlankBoatsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLANKBOATS_API ABlankBoatsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
